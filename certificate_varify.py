d67e68b46c497d691505417dbc291c23d008e280
melisgibson <melcassidy45@gmail.com>
2022-12-02 13:50:49 -0600

    update

---
+++ certificate_varify.py
@@ -0,0 +1,63 @@
+import ssl
+import socket
+import datetime
+import boto3
+
+client = boto3.client("ses", region_name="us-east-1")
+
+print(f"Program to check SSL certificate validity and expiration date\n")
+
+##opening file
+with open("server_ip.txt") as ip_file:
+
+    ##check  certificate expiration
+    for ip in ip_file:
+
+        try:
+            host, port = ip.strip().split(":")
+            print(f"\nChecking certifcate for server {host}")
+            context = ssl.create_default_context()
+            with socket.create_connection((host, port)) as sock:
+                with context.wrap_socket(sock, server_hostname=host) as ssock:
+                    certificate = ssock.getpeercert()
+                certExpires = datetime.datetime.strptime(
+                    certificate["notAfter"], "%b %d %H:%M:%S %Y %Z"
+                )
+                daysToExpiration = (certExpires - datetime.datetime.now()).days
+                print(f"Expires on: {certExpires} in {daysToExpiration} days")
+                ##preparing mailbody
+                mailbody = (
+                    "Server name: "
+                    + host
+                    + ", expires in "
+                    + str(daysToExpiration)
+                    + " days."
+                )
+
+        except:
+            print(f"error on connection to Server, {host}")
+
+        ##sending ses email
+        if daysToExpiration < 45:
+            response = client.send_email(
+                Destination={
+                    "ToAddresses": ["melcassidy45@gmail.com"],
+                },
+                Message={
+                    "Body": {
+                        "Text": {
+                            "Charset": "UTF-8",
+                            "Data": "The following requires attention; "
+                            + mailbody
+                            + "\nThank you.",
+                        },
+                    },
+                    "Subject": {
+                        "Charset": "UTF-8",
+                        "Data": "Certificate Expiring Soon",
+                    },
+                },
+                Source="melcassidy45@gmail.com",
+            )
+
+print(f"\nCert check complete!")
